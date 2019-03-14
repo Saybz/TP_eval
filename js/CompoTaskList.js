f723c5c45fc9d6cfa997f9d1862dad4ff3961421
Saybz <shailash.bhati@gmail.com>
2019-03-14 09:50:11 +0100

    Rédaction de la JSDoc du fichier : CompoTaskList.js

--- js/CompoTaskList.js
+++ js/CompoTaskList.js
@@ -1,15 +1,45 @@
 class CompoTaskList {
+    /** 
+     * @class
+     * @summary Composant qui affiche une liste
+     * Les paramètres obligatoire, transmettre sous la forme
+     * d'un objet, sont :
+     * - tasklist : objet TaskList à afficher
+     * - element : objet HTMLElement du DOM dans lequel
+     *   le code HTML doit être affiché
+     * @param {*} param - Object de paramétrage
+     * @example
+     * let tl = new TaskList('test', null);
+     * let compoTk = new CompoTask({
+     *    taskList: tl,
+     *    element: document.getElementById('tastlist')
+     * })
+     * compoTk.render();
+     */
     constructor(param) {
         this.param = param;
     }
+
+    /**
+     * Retourne la liste passée en paramètre au constructeur
+     */
 
     getTaskList() {
         return this.param.taskList;
     }
 
+    /**
+     * Retourne l'élément du DOM passé en paramètre au constructeur
+     */
+
     getElement() {
         return this.param.element;
     }
+
+    /**
+     * Affiche la liste dans l'élément du DOM en utilisant un bloc div
+     * avec la classe CSS "tasklist"
+     */
 
     render() {
         let children = [];
